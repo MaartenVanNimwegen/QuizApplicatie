8eac1764c583ff1df0c064dfcae3aee7b031b0af
Jesper <jesperminks7@gmail.com>
2022-03-14 14:19:32 +0100

    Achtergrond kleur gezet

--- QuizApplicatie/QuizApplicatie/Form1.Designer.cs
+++ QuizApplicatie/QuizApplicatie/Form1.Designer.cs
@@ -29,10 +29,18 @@ namespace QuizApplicatie
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(114)))), ((int)(((byte)(185)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
