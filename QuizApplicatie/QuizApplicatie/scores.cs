233748e420c06e656b71136e5e47df9bd901c5a0
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2022-03-24 16:01:43 +0100

    eind dg

--- QuizApplicatie/QuizApplicatie/scores.cs
+++ QuizApplicatie/QuizApplicatie/scores.cs
@@ -14,8 +14,6 @@ namespace QuizApplicatie
     public partial class scores : Form
     {
         int antwoordId = 0;
-        int userId = 0;
-
         public scores()
         {
             InitializeComponent();
@@ -26,35 +24,9 @@ namespace QuizApplicatie
             AntwoordGrid.Rows.Clear();
 
             string query = "SELECT * FROM antwoord";
-            string query2 = "SELECT naam FROM speler WHERE 'id' = '" + userId + "'";
-            string query3 = "SELECT * FROM vragen";
-
+            
             var Antwoorden = new List<AntwoordenClass>();
 
-            using (MySqlConnection connection = new MySqlConnection())
-            {
-                connection.ConnectionString = "Data Source = localhost; Initial Catalog = quizapplicatie; User ID = root; Password = ";
-                using (MySqlCommand command = new MySqlCommand(query, connection))
-                {
-                    connection.Open();
-                    MySqlDataReader reader = command.ExecuteReader();
-
-                    if (reader.HasRows)
-                    {
-                        while (reader.Read())
-                        {
-                            userId = reader.GetInt32(1);
-                            using (MySqlCommand command2 = new MySqlCommand(query2, connection))
-                            {
-                                reader.Close();
-                                MySqlDataReader reader2 = command2.ExecuteReader();
-                            }
-
-                        }
-                    }
-                    reader.Close();
-                }
-            }
             using (MySqlConnection connection = new MySqlConnection())
             {
                 connection.ConnectionString = "Data Source = localhost; Initial Catalog = quizapplicatie; User ID = root; Password = ";
@@ -70,25 +42,15 @@ namespace QuizApplicatie
                             AntwoordenClass LeAntwoord = new AntwoordenClass();
                             LeAntwoord.id = reader.GetInt32(0);
                             LeAntwoord.userId = reader.GetInt32(1);
-                            LeAntwoord.vraagId= reader.GetInt32(2);
+                            LeAntwoord.vraagId = reader.GetInt32(2);
                             LeAntwoord.tijd = reader.GetInt32(3);
-                            LeAntwoord.strafTijd= reader.GetInt32(4);
+                            LeAntwoord.strafTijd = reader.GetInt32(4);
                             LeAntwoord.IsGoedBeantwoord = reader.GetInt32(5);
 
                             Antwoorden.Add(LeAntwoord);
                         }
                     }
                     reader.Close();
-                }
-            }
-
-            for (int i = 1; i <= Antwoorden.Count; i++)
-            {
-                var Antwoord = Antwoorden[i - 1];
-
-                if (Antwoord != null)
-                {
-                    AntwoordGrid.Rows.Add(Antwoord.id, Antwoord.userId, Antwoord.vraagId, Antwoord.tijd, Antwoord.strafTijd, Antwoord.IsGoedBeantwoord, Antwoord.datum);
                 }
             }
         }
